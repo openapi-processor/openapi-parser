16216a7d202c0fd0332e8ec9f67f9fd2ea2af3e6
Martin Hauner <martin.hauner@gmx.net>
2021-09-19 18:53:32 +0200

    validate required properties

---
+++ openapi-parser/src/main/java/io/openapiparser/validations/RequiredValidation.java
@@ -0,0 +1,33 @@
+package io.openapiparser.validations;
+
+import io.openapiparser.*;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * validates existence of required properties on the given node.
+ */
+class RequiredValidation implements Validation {
+    private final Collection<String> properties;
+
+    public RequiredValidation (Collection<String> properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public Collection<ValidationMessage> validate (ValidationContext context, Node node) {
+        return properties.stream ()
+            .filter (prop -> !node.containsKey (prop))
+            .map (prop -> createMessage (context, prop))
+            .collect(Collectors.toList());
+    }
+
+    private ValidationMessage createMessage (ValidationContext context, String property) {
+        return new ValidationMessage(context.getPropertyPath (property), createText (property));
+    }
+
+    private String createText(String property) {
+        return String.format ("'%s' is a missing but required property", property);
+    }
+}
