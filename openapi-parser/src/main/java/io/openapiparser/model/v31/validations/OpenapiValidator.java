4ac5127f116e7fe7d89898aa44d86bacc14ec7fe
Martin Hauner <martin.hauner@gmx.net>
2021-09-28 09:17:54 +0200

    validate openapi object

--- openapi-parser/src/main/java/io/openapiparser/model/v31/validations/OpenapiValidator.java
+++ openapi-parser/src/main/java/io/openapiparser/model/v31/validations/OpenapiValidator.java
@@ -9,12 +9,13 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 import static io.openapiparser.model.v31.Keywords.OPENAPI_KEYS;
+import static io.openapiparser.model.v31.Keywords.OPENAPI_KEYS_REQUIRED;
 import static io.openapiparser.validations.AllowedPropertiesValidator.Extensions.INCLUDE_X;
 
 public class OpenapiValidator implements Validator {
     private final Collection<Validator> validations = Arrays.asList(
-        new AllowedPropertiesValidator (Keywords.OPENAPI_KEYS, INCLUDE_X),
-        new RequiredPropertiesValidator (OPENAPI_KEYS),
+        new AllowedPropertiesValidator (OPENAPI_KEYS, INCLUDE_X),
+        new RequiredPropertiesValidator (OPENAPI_KEYS_REQUIRED),
 //        new RequiredPropertiesXorValidator (OPENAPI_KEYS_REQUIRED_XOR)
         new VersionValidator ()
     );
---
+++ openapi-parser/src/test/kotlin/io/openapiparser/model/v30/validations/OpenapiValidatorSpec.kt
@@ -0,0 +1,37 @@
+package io.openapiparser.model.v30.validations
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldExist
+import io.kotest.matchers.shouldBe
+import io.openapiparser.ValidationContext
+import io.openapiparser.support.TestBuilder
+import io.openapiparser.support.matches
+import java.net.URI
+
+class OpenapiValidatorSpec : StringSpec({
+
+    "validates 'openapi' object" {
+        val ctx = TestBuilder()
+            .withApi("""
+                openapi: 3
+                x-foo: allowed
+                bad: not allowed
+            """.trimIndent())
+            .buildContext()
+        ctx.read()
+
+        val validator = OpenapiValidator()
+        val messages = validator.validate(
+            ValidationContext(URI("file:///any")), ctx.baseNode)
+
+        messages.size shouldBe 4
+        // version
+        messages.shouldExist { it.matches("$.openapi", "'3'") }
+        // required
+        messages.shouldExist { it.matches("$.info", "'info'") }
+        messages.shouldExist { it.matches("$.paths", "'paths'") }
+        // not allowed
+        messages.shouldExist { it.matches("$.bad", "'bad'") }
+    }
+
+})
