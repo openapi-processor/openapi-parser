123743b8faa8f67eb33ddecc98f0836d046a792b
Martin Hauner <martin.hauner@gmx.net>
2021-10-17 17:22:34 +0200

    extract jackson converter

--- openapi-parser-jackson/build.gradle.kts
+++ openapi-parser-jackson/build.gradle.kts
@@ -3,5 +3,9 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":openapi-parser-api"))
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.12.5"))
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+
+    implementation(project(":openapi-parser-base"))
 }
