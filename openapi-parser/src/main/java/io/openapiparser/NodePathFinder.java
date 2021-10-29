39ff55549dd893397abb4b21006d8bf84d4a8459
Martin Hauner <martin.hauner@gmx.net>
2021-10-29 15:15:14 +0200

    find fragment path of node

---
+++ openapi-parser/src/main/java/io/openapiparser/NodePathFinder.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser;
+
+public class NodePathFinder {
+    private static final String SLASH =  "/";
+
+    private final Node root;
+
+    NodePathFinder (Node root) {
+        this.root = root;
+    }
+
+    Object find(String path) {
+        if (!isPath (path)) {
+            return root.get (path);
+        } else {
+            String[] parts = getParts (path);
+            int last = parts.length - 1;
+
+            Node node = root;
+            for (int i = 0; i < parts.length; i++) {
+                String part = parts[i];
+
+                if (!node.containsKey (part))
+                    return null;
+
+                if (i == last)
+                    return node.get (part);
+
+                node = node.getChildNode (part);
+            }
+
+            return null;
+        }
+    }
+
+    private boolean isPath (String path) {
+        return path.startsWith (SLASH);
+    }
+
+    private String[] getParts (String path) {
+        return path.substring (1).split (SLASH);
+    }
+}
---
+++ openapi-parser/src/test/kotlin/io/openapiparser/NodePathFinderSpec.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+class NodePathFinderSpec : StringSpec({
+
+    "returns null for unknown path" {
+        NodePathFinder(Node(mapOf())).find("/unknown").shouldBeNull()
+    }
+
+    "find by single level path" {
+        val found = NodePathFinder(Node(mapOf("key" to "value"))).find("/key")
+
+        found.shouldBeInstanceOf<String>()
+        found.shouldBe("value")
+    }
+
+    "find by multi level path" {
+        val found = NodePathFinder(Node(
+
+            mapOf("level 0" to
+                mapOf("level 1" to
+                    mapOf("level 2" to
+                        mapOf("target" to
+                            mapOf<String, Any>("key" to "value")))))
+        )).find("/level 0/level 1/level 2/target")
+
+        found.shouldBeInstanceOf<Map<String, Any>>()
+        found["key"].shouldBe("value")
+    }
+
+})
