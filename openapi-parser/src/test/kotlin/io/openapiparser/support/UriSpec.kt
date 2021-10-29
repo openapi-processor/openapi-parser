870f5b0da1e0b0cc6ba0550988bdc995e4550626
Martin Hauner <martin.hauner@gmx.net>
2021-10-29 15:11:02 +0200

    resolve relative uri

---
+++ openapi-parser/src/test/kotlin/io/openapiparser/support/UriSpec.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.support
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.net.URI
+
+class UriSpec: StringSpec({
+
+    // the slash is required, otherwise the uri is "opaque" and relative resolving does not work.
+    "resolve relative memory uri" {
+        URI("memory:/foo").resolve("bar").toString() shouldBe "memory:/bar"
+    }
+
+})
