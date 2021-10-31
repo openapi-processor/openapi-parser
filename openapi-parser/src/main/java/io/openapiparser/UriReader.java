600081e21ab182276b155959aae7e2dd2cc13e73
Martin Hauner <martin.hauner@gmx.net>
2021-10-31 18:47:19 +0100

    javadoc

--- openapi-parser/src/main/java/io/openapiparser/UriReader.java
+++ openapi-parser/src/main/java/io/openapiparser/UriReader.java
@@ -9,8 +9,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.*;
 
+/**
+ * simple {@link Reader} implementation based on {@link URL}.
+ */
 public class UriReader implements Reader {
 
+    /**
+     * reads the document at the given uri.
+     *
+     * @param uri uri of the OpenAPI document.
+     * @return input stream of the uri
+     * @throws IOException if it fails to read the uri
+     */
     @Override
     public InputStream read (URI uri) throws IOException {
         URL root = uri.toURL ();
