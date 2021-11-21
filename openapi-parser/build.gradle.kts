18af2ac22592b6540ca20bcdeb72d9a158a191f8
Martin Hauner <martin.hauner@gmx.net>
2021-11-21 18:01:04 +0100

    need spock

--- openapi-parser/build.gradle.kts
+++ openapi-parser/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    groovy
     id("openapi-parser.java-conventions")
     id("org.unbroken-dome.test-sets") version "4.0.0"
     id("com.github.ben-manes.versions") version "0.39.0"
@@ -6,5 +7,16 @@ plugins {
 
 dependencies {
     implementation(project(":openapi-parser-base"))
+
     testImplementation(project(":openapi-parser-jackson"))
+    testImplementation("org.spockframework:spock-core:2.1-M2-groovy-3.0")
 }
+
+tasks {
+    // groovy tests depend on kotlin tests
+    compileTestGroovy {
+        dependsOn(compileTestKotlin)
+        classpath += files(compileTestKotlin)
+    }
+}
+
