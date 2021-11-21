b75bedfc8740021377850767de00b6d4de7f1271
Martin Hauner <martin.hauner@gmx.net>
2021-11-21 18:04:07 +0100

    simpler

--- openapi-parser/src/test/kotlin/io/openapiparser/model/v31/OpenApiSpec.kt
+++ openapi-parser/src/test/kotlin/io/openapiparser/model/v31/OpenApiSpec.kt
@@ -14,130 +14,59 @@ import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.openapiparser.NoValueException
 import io.openapiparser.support.TestBuilder
+import io.openapiparser.support.buildObject
 
-class OpenApiSpec  : StringSpec({
+class OpenApiSpec: StringSpec({
 
     "gets openapi version" {
-        val api = TestBuilder()
-            .withApi("""
-                openapi: 3.1.0
-            """.trimIndent())
-            .buildOpenApi31()
-
-        api.openapi shouldBe "3.1.0"
+        openapi("openapi: 3.1.0").openapi shouldBe "3.1.0"
     }
 
     "gets openapi version throws if it is missing" {
-        val api = TestBuilder()
-            .withApi("""
-                any: value
-            """.trimIndent())
-            .buildOpenApi31()
-
-        shouldThrow<NoValueException> {
-            api.openapi
-        }
+        shouldThrow<NoValueException> { openapi().openapi }
     }
 
     "gets info object" {
-        val api = TestBuilder()
-            .withApi("""
-                info: {}
-            """.trimIndent())
-            .buildOpenApi31()
-
-        api.info.shouldNotBeNull()
+        openapi("info: {}").info.shouldNotBeNull()
     }
 
     "gets info object throws if it missing" {
-        val api = TestBuilder()
-            .withApi("""
-                any: value
-            """.trimIndent())
-            .buildOpenApi31()
-
-        shouldThrow<NoValueException> {
-            api.info
-        }
+        shouldThrow<NoValueException> { openapi().info }
     }
 
     "gets json schema dialect" {
-        val api = TestBuilder()
-            .withApi("""
-                jsonSchemaDialect: https://schema.uri
-            """.trimIndent())
-            .buildOpenApi31()
-
-        api.jsonSchemaDialect.shouldNotBeNull()
+        openapi("jsonSchemaDialect: https://schema.uri").jsonSchemaDialect.shouldNotBeNull()
     }
 
     "gets null json schema dialect if it missing" {
-        val api = TestBuilder()
-            .withApi("""
-                any: value
-            """.trimIndent())
-            .buildOpenApi31()
-
-        api.externalDocs.shouldBeNull()
+        openapi().jsonSchemaDialect.shouldBeNull()
     }
 
     "gets server objects" {
-        val api = TestBuilder()
-            .withApi("""
-                servers:
-                  - {}
-                  - {}
-            """.trimIndent())
-            .buildOpenApi31()
-
-        val servers = api.servers
+        val servers = openapi("servers: [{}, {}]").servers
         servers.shouldNotBeNull()
-        servers.size shouldBe 2
+        servers.size
     }
 
     "gets empty server objects if it is missing" {
-        val api = TestBuilder()
-            .withApi("""
-                openapi: 3.1.0
-            """.trimIndent())
-            .buildOpenApi31()
-
-        val servers = api.servers
-        servers.shouldBeEmpty()
+        openapi().servers.shouldBeEmpty()
     }
 
     "gets paths object" {
-        val api = TestBuilder()
-            .withApi(
-                """
-                paths: {}
-            """.trimIndent()
-            )
-            .buildOpenApi31()
-
-        api.paths.shouldNotBeNull()
+        openapi("paths: {}").paths.shouldNotBeNull()
     }
 
     "gets path object is null if it missing" {
-        val api = TestBuilder()
-            .withApi("""
-                any: value
-            """.trimIndent())
-            .buildOpenApi31()
-
-        api.paths.shouldBeNull()
+        openapi().paths.shouldBeNull()
     }
 
     "gets webhooks objects" {
-        val api = TestBuilder()
-            .withApi("""
-                webhooks:
-                  /foo: {}
-                  /bar: {}
-            """.trimIndent())
-            .buildOpenApi31()
+        val webhooks = openapi("""
+            webhooks:
+              /foo: {}
+              /bar: {}
+        """).webhooks
 
-        val webhooks = api.webhooks
         webhooks.size shouldBe 2
         webhooks["/foo"].shouldNotBeNull()
         webhooks["/bar"].shouldNotBeNull()
@@ -249,3 +178,7 @@ class OpenApiSpec  : StringSpec({
         extensions.size shouldBe 2
     }
 })
+
+fun openapi(content: String = "{}"): OpenApi {
+    return buildObject(OpenApi::class.java, content)
+}
