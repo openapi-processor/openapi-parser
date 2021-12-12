ecb1e779d521c9bd86abf6e18d3b23a416c25acc
Martin Hauner <martin.hauner@gmx.net>
2021-12-12 18:42:20 +0100

    placeholder

---
+++ openapi-parser/src/test/kotlin/io/openapiparser/model/v3x/ExampleSpec.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.model.v3x
+
+import io.kotest.core.spec.style.StringSpec
+
+class ExampleSpec: StringSpec({
+    // todo
+})
