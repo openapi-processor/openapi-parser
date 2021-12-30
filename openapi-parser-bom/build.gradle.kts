c241dedbed92ddff5c40b91758777f4de9d5f8ee
Martin Hauner <martin.hauner@gmx.net>
2021-12-30 11:44:40 +0100

    gradle platform bom

---
+++ openapi-parser-bom/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `java-platform`
+}
+
+dependencies {
+    constraints {
+        api(project(":openapi-parser"))
+        api(project(":openapi-parser-base"))
+        api(project(":openapi-parser-interfaces"))
+        api(project(":openapi-parser-jackson"))
+        api(project(":openapi-parser-validator"))
+        api(project(":openapi-parser-shared"))
+    }
+}
