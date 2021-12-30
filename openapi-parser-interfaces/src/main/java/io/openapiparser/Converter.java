bfa4e81b4bd76912b48c98bc8e9788292b96d9b6
Martin Hauner <martin.hauner@gmx.net>
2021-12-30 11:50:45 +0100

    better home

---
+++ openapi-parser-interfaces/src/main/java/io/openapiparser/Converter.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser;
+
+/**
+ * yaml/json parser abstraction.
+ */
+public interface Converter {
+
+    /**
+     * converts a source (json/yaml) document into a java object tree. The tree structure is based
+     * on {@code Map<String, Object>}. The root may not be map if the document just contains a
+     * single value.
+     *
+     * @param api a json/yaml document.
+     * @return object tree of the document.
+     */
+    Object convert (String api) throws ConverterException;
+}
