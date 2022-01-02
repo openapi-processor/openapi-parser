a978565ea8736cc9977e6ee4d5a9be5d8cc4ec71
Martin Hauner <martin.hauner@gmx.net>
2022-01-02 16:24:17 +0100

    better name

--- openapi-parser/src/main/java/io/openapiparser/Properties.java
+++ openapi-parser/src/main/java/io/openapiparser/Properties.java
@@ -42,7 +42,7 @@ public class Properties {
     /* Strings */
 
     protected @Nullable String getStringOrNull (String property) {
-        return bucket.convert (property, new StringOrNullConverter ());
+        return bucket.convert (property, new StringNullableConverter ());
     }
 
     protected String getStringOrThrow (String property) {
