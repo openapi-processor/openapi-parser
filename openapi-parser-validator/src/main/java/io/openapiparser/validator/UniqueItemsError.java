b3523f8ee463668ae584b234a68642f396c3269b
Martin Hauner <martin.hauner@gmx.net>
2022-01-03 11:20:21 +0100

    doc

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/UniqueItemsError.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/UniqueItemsError.java
@@ -1,5 +1,13 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
 package io.openapiparser.validator;
 
+/**
+ * draft4 - 5.12
+ */
 public class UniqueItemsError extends ValidationMessage {
     public UniqueItemsError (String path) {
         super (path, "the items are not unique");
