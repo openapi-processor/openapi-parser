3c8316f1bc35dde2cbc77c0b463cc4c1d012949e
Martin Hauner <martin.hauner@gmx.net>
2022-01-08 14:29:42 +0100

    ref test case

--- openapi-parser/src/test/kotlin/io/openapiparser/RefSpec.kt
+++ openapi-parser/src/test/kotlin/io/openapiparser/RefSpec.kt
@@ -27,5 +27,29 @@ class RefSpec: StringSpec({
         ref.properties["bar"]!!.type shouldBe "string"
     }
 
+    "ref-array-items-nested" {
+        val api = ApiBuilder()
+            .withResource("/v30/ref-array-items-nested/openapi.yaml")
+            .buildOpenApi30()
+
+        val schema = api.getResponseSchema("/array", "200", "application/json")
+        schema.type shouldBe "array"
+
+        val bar = schema.items
+        bar!!.ref shouldBe "#/components/schemas/Bar"
+
+        val refBar = bar.refObject
+        val foos = refBar.properties["foos"]
+        foos!!.type shouldBe "array"
+
+        val foo = foos.items
+        foo!!.ref shouldBe "#/components/schemas/Foo"
+
+        val fooRef = foo.refObject
+        fooRef.type shouldBe "object"
+        fooRef.properties.size shouldBe 1
+        fooRef.properties["foo"]!!.type shouldBe "string"
+    }
+
 })
 
