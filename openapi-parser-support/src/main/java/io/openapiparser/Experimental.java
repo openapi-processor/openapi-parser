18d3a2cd9dc819efc64dafcf7198508e6966f393
Martin Hauner <martin.hauner@gmx.net>
2022-01-08 17:22:34 +0100

    doc support

---
+++ openapi-parser-support/src/main/java/io/openapiparser/Experimental.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser;
+
+import java.lang.annotation.*;
+
+/**
+ * marks experimental apis that should not (yet..) be used in production code. It is used for
+ * documentation only.
+ */
+@Documented
+@Retention (RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Experimental {}
