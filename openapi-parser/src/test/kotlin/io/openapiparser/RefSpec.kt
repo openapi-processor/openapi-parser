5eaf9e51bd7eb6200930acf3bb4a6d4ebdbe4ef4
Martin Hauner <martin.hauner@gmx.net>
2022-01-08 11:14:25 +0100

    simpler

--- openapi-parser/src/test/kotlin/io/openapiparser/RefSpec.kt
+++ openapi-parser/src/test/kotlin/io/openapiparser/RefSpec.kt
@@ -8,6 +8,7 @@ package io.openapiparser
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.openapiparser.support.ApiBuilder
+import io.openapiparser.support.getResponseSchema
 
 class RefSpec: StringSpec({
 
@@ -16,11 +17,10 @@ class RefSpec: StringSpec({
             .withResource("/v30/ref-into-another-file/openapi.yaml")
             .buildOpenApi30()
 
-        val pathItem = api.paths.getPathItem("/foo")
-        val response = pathItem!!.get!!.responses.getResponse("200")
-        val schema = response!!.content["application/json"]!!.schema
+        val schema = api.getResponseSchema("/foo", "200", "application/json")
 
-        schema!!.ref shouldBe "foo.yaml#/Foo"
+        schema.ref shouldBe "foo.yaml#/Foo"
+
         val ref = schema.refObject
         ref.type shouldBe "object"
         ref.properties.size shouldBe 1
---
+++ openapi-parser/src/test/kotlin/io/openapiparser/support/OpenApi.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2022 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.support
+
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.openapiparser.model.v30.OpenApi as OpenApi30
+import io.openapiparser.model.v30.Schema as Schema30
+
+fun OpenApi30.getResponseSchema(path: String, status: String, media: String): Schema30 {
+    val pathItem = paths.getPathItem(path)
+    pathItem.shouldNotBeNull()
+
+    val operation = pathItem.get
+    operation.shouldNotBeNull()
+
+    val response = operation.responses.getResponse(status)
+    response.shouldNotBeNull()
+
+    val content = response.content[media]
+    content.shouldNotBeNull()
+
+    val schema = content.schema
+    schema.shouldNotBeNull()
+
+    return schema
+}
