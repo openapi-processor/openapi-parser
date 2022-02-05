9546a798b3c1b3b6a877a9bb50f60b5f97c95fdd
Martin Hauner <martin.hauner@gmx.net>
2022-02-05 12:25:39 +0100

    simple document storage

---
+++ openapi-parser-validator/src/main/java/io/openapiparser/schema/DocumentStore.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.schema;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * repository for "downloaded" documents. Used to cache documents and avoid downloading the same
+ * document multiple times.
+ *
+ * a document may be a single value or a whole object tree.
+ */
+public class DocumentStore {
+    private final Map<URI, Object> documents = new HashMap<> ();
+
+    public void add (URI uri, Object document) {
+        documents.put (uri, document);
+    }
+
+    public Object get (URI uri) {
+        final Object document = documents.get (uri);
+        if (document == null)
+            // todo
+            throw new RuntimeException ();
+
+        return document;
+    }
+
+    public boolean contains (URI uri) {
+        return documents.containsKey (uri);
+    }
+}
---
+++ openapi-parser-validator/src/test/kotlin/io/openapiparser/schema/DocumentStoreSpec.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.schema
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import java.net.URI
+
+class DocumentStoreSpec : StringSpec({
+
+    "stores document" {
+        val ds = DocumentStore()
+        val uri = URI.create("https://any")
+
+        val document = emptyMap<String, Any>()
+        ds.add(uri, document)
+
+        ds.contains(uri).shouldBeTrue()
+        ds.get(uri).shouldBe(document)
+    }
+
+})
