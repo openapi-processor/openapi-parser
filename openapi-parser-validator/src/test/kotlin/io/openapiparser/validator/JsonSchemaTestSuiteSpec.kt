8ab06430e7400e557d24894e0eca0eddff79cd0e
Martin Hauner <martin.hauner@gmx.net>
2022-03-01 09:01:40 +0100

    add unsupported

--- openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/JsonSchemaTestSuiteSpec.kt
+++ openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/JsonSchemaTestSuiteSpec.kt
@@ -10,6 +10,10 @@ import io.openapiparser.validator.support.draftSpec
 
 class JsonSchemaTestSuiteSpec: FreeSpec ({
     include(draftSpec("/suites/JSON-Schema-Test-Suite/tests/draft4", arrayOf(
-        "ecmascript-regex.json"  // (optional) not supported
+        // (optional) ecmascript-regex.json, too many fails to list them
+        "ecmascript-regex.json",
+        // (optional) date-time.json, supports all except leap second
+        "a valid date-time with a leap second, UTC",
+        "a valid date-time with a leap second, with minus offset",
     )))
 })
