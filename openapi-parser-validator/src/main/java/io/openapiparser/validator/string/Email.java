6c0973ee20ef2b205e7e261b084faecc9068a756
Martin Hauner <martin.hauner@gmx.net>
2022-04-03 16:45:25 +0200

    step migration email

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/string/Email.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/string/Email.java
@@ -7,10 +7,9 @@ package io.openapiparser.validator.string;
 
 import io.openapiparser.schema.JsonInstance;
 import io.openapiparser.schema.JsonSchema;
-import io.openapiparser.validator.ValidationMessage;
+import io.openapiparser.validator.steps.NullStep;
+import io.openapiparser.validator.steps.ValidationStep;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.regex.Matcher;
 
 /**
@@ -20,6 +19,8 @@ import java.util.regex.Matcher;
  * <a href="https://datatracker.ietf.org/doc/html/draft-fge-json-schema-validation-00#section-7.3.2">
  *     Draft 4: email
  * </a>
+ *
+ * todo replace regex with validation code
  */
 public class Email {
     // https://stackoverflow.com/a/48725527
@@ -27,23 +28,19 @@ public class Email {
         "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*" +
         "@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
 
-    public Collection<ValidationMessage> validate (
-        JsonSchema schema, JsonInstance instance) {
-
-        Collection<ValidationMessage> messages = new ArrayList<> ();
-
+    public ValidationStep validate (JsonSchema schema, JsonInstance instance) {
         String format = schema.getFormat ();
         if (format == null || !format.equals ("email"))
-            return messages;
+            return new NullStep ();
 
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(EMAIL_REGEX);
         String instanceValue = instance.asString ();
         Matcher m = p.matcher(instanceValue);
         boolean valid = m.find ();
-        if (!valid) {
-            messages.add (new EmailError (instance.getPath ()));
-        }
 
-        return messages;
+        if (valid)
+            return new EmailStep ();
+
+        return new EmailStep (new EmailError (instance.getPath ()));
     }
 }
---
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/string/EmailStep.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2022 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.validator.string;
+
+import io.openapiparser.validator.ValidationMessage;
+import io.openapiparser.validator.steps.SimpleStep;
+
+public class EmailStep extends SimpleStep {
+
+    public EmailStep () {
+        super ();
+    }
+
+    public EmailStep (ValidationMessage message) {
+        super(message);
+    }
+}
