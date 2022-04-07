7edffc2b60eb59ea91cb6b9cd12eab81a288cf27
Martin Hauner <martin.hauner@gmx.net>
2022-04-07 09:16:57 +0200

    simpler & more info

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/ValidationMessage.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/ValidationMessage.java
@@ -5,20 +5,66 @@
 
 package io.openapiparser.validator;
 
+import io.openapiparser.schema.JsonInstance;
+import io.openapiparser.schema.JsonSchema;
+
+// todo interface
 public class ValidationMessage {
-    // kind/source ?
-    // warn/error ?
-    // file ?
-    private final String path;
     private final String text;
 
+    private final JsonSchema schema;
+    private final JsonInstance instance;
+
+    private final String scope;
+    private final String path;
+
     public ValidationMessage (String path, String text) {
-        this.path = path;
         this.text = text;
+        this.schema = null;
+        this.instance = null;
+        this.scope = "?";
+        this.path = path;
+    }
+    public ValidationMessage (String scope, String path, String text) {
+        this.text = text;
+        this.schema = null;
+        this.instance = null;
+        this.scope = scope;
+        this.path = path;
     }
 
+    public ValidationMessage (JsonSchema schema, JsonInstance instance, String text) {
+        this.text = text;
+        this.schema = schema;
+        this.instance = instance;
+        this.scope = instance.getScope ().toString ();
+        this.path = instance.getPath ();
+    }
+
+    @Deprecated
+    public String getScope () {
+        return scope;
+    }
+
+    @Deprecated
     public String getPath () {
         return path;
+    }
+
+    public String getSchemaScope () {
+        return schema.getContext ().getScope ().toString ();
+    }
+
+    public String getSchemaPath () {
+        return schema.getLocation ().toString ();
+    }
+
+    public String getInstanceScope () {
+        return instance.getScope ().toString ();
+    }
+
+    public String getInstancePath () {
+        return instance.getPath ();
     }
 
     public String getText () {
