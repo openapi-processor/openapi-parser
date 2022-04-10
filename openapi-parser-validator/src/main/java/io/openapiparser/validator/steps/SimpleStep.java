27b21997efd3124fbf1900cb7f2e8f5be9a0bfa9
Martin Hauner <martin.hauner@gmx.net>
2022-04-10 17:50:43 +0200

    cleanup

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/steps/SimpleStep.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/steps/SimpleStep.java
@@ -8,8 +8,6 @@ package io.openapiparser.validator.steps;
 import io.openapiparser.schema.JsonInstance;
 import io.openapiparser.schema.JsonSchema;
 import io.openapiparser.validator.ValidationMessage;
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -19,38 +17,16 @@ public abstract class SimpleStep implements ValidationStep {
     protected final JsonInstance instance;
     private boolean valid = true;
 
-    @Deprecated
-    private final @Nullable ValidationMessage message;
-
-
-    @Deprecated
-    public SimpleStep () {
-        this.schema = null;
-        this.instance = null;
-        this.message = null;
-    }
-    
-    @Deprecated
-    public SimpleStep (@NonNull ValidationMessage message) {
-        this.schema = null;
-        this.instance = null;
-        this.message = message;
-    }
-
     public SimpleStep (JsonSchema schema, JsonInstance instance) {
         this.schema = schema;
         this.instance = instance;
-        this.message = null;
     }
 
     public void setInvalid () {
         valid = false;
     }
 
-    // todo make abstract
-    protected /*abstract*/ ValidationMessage getError () {
-        return message;
-    };
+    protected abstract ValidationMessage getError ();
 
     @Override
     public Collection<ValidationStep> getSteps () {
