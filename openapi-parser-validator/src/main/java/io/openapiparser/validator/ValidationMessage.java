188ac781a635f5cdb38d0bf66b18c9249e0247cc
Martin Hauner <martin.hauner@gmx.net>
2022-04-11 08:10:06 +0200

    cleanup

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/ValidationMessage.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/ValidationMessage.java
@@ -19,33 +19,10 @@ public class ValidationMessage {
 
     private Collection<ValidationMessage> nestedMessage = Collections.emptyList ();
 
-    @Deprecated private final String scope;
-    @Deprecated private final String path;
-
-    @Deprecated
-    public ValidationMessage (String path, String text) {
-        this.text = text;
-        this.schema = null;
-        this.instance = null;
-        this.scope = "?";
-        this.path = path;
-    }
-
-    @Deprecated
-    public ValidationMessage (String scope, String path, String text) {
-        this.text = text;
-        this.schema = null;
-        this.instance = null;
-        this.scope = scope;
-        this.path = path;
-    }
-
     public ValidationMessage (JsonSchema schema, JsonInstance instance, String text) {
         this.text = text;
         this.schema = schema;
         this.instance = instance;
-        this.scope = instance.getScope ().toString ();
-        this.path = instance.getPath ();
     }
 
     public ValidationMessage (
@@ -57,19 +34,7 @@ public class ValidationMessage {
         this.text = text;
         this.schema = schema;
         this.instance = instance;
-        this.scope = instance.getScope ().toString ();
-        this.path = instance.getPath ();
         this.nestedMessage = nestedMessage;
-    }
-
-    @Deprecated
-    public String getScope () {
-        return scope;
-    }
-
-    @Deprecated
-    public String getPath () {
-        return path;
     }
 
     public String getSchemaScope () {
