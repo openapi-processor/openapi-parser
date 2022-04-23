19072b6f1fab8e9b53a614300137444e9a546235
Martin Hauner <martin.hauner@gmx.net>
2022-04-23 09:35:27 +0200

    configure schema version

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/ValidatorSettings.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/ValidatorSettings.java
@@ -5,5 +5,32 @@
 
 package io.openapiparser.validator;
 
+import io.openapiparser.schema.SchemaVersion;
+
 public class ValidatorSettings {
+    private SchemaVersion version = SchemaVersion.Draft6;
+
+    public boolean isDraft4 () {
+        return SchemaVersion.Draft4.equals (version);
+    }
+
+    /**
+     * get the JSON schema version that should be used if a schema doesn't provide a $schema. The
+     * default is {@code Version.Latest}.
+     *
+     * @return the JSON schema version.
+     */
+    public SchemaVersion getVersion () {
+        return version;
+    }
+
+    /**
+     * set a JSON schema version. It is a fallback version that is used if a schema doesn't provide
+     * a $schema.
+     *
+     * @param version JSON schema version
+     */
+    public void setVersion (SchemaVersion version) {
+        this.version = version;
+    }
 }
