08fb66c8717baa6390923b3e49682dfe6af36fdc
Martin Hauner <martin.hauner@gmx.net>
2022-05-01 11:29:29 +0200

    clean up

--- openapi-parser/src/test/kotlin/io/openapiparser/ValidateSpec.kt
+++ openapi-parser/src/test/kotlin/io/openapiparser/ValidateSpec.kt
@@ -8,30 +8,37 @@ package io.openapiparser
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.openapiparser.reader.UriReader
+import io.openapiparser.schema.DocumentStore
+import io.openapiparser.schema.Resolver
 import io.openapiparser.schema.SchemaStore
 import io.openapiparser.snakeyaml.SnakeYamlConverter
-import io.openapiparser.support.ApiBuilder
-import io.openapiparser.validator.Validator
-import java.net.URI
+import io.openapiparser.validator.*
+import io.openapiparser.validator.result.FullResultTextBuilder
+import io.openapiparser.validator.result.ListResultBuilder
 
 class ValidateSpec: StringSpec({
 
-    "validate" {
-        val parser = ApiBuilder()
-            .withResource("/v30/ref-into-another-file/openapi.yaml")
-            .buildParser()
+    val resolver = Resolver(UriReader(), SnakeYamlConverter(), DocumentStore())
+    val store = SchemaStore(resolver)
 
-        val result = parser.parse()
+    fun draft4() {
+        store.loadDraft4()
+    }
+
+    "validate openapi with \$ref into another file" {
+        draft4()
+
+        val parser = OpenApiParser(resolver)
+        val result = parser.parse("/v30/ref-into-another-file/openapi.yaml")
+
         val validator = Validator()
+        val valid = result.validate(validator, store)
 
-        val store = SchemaStore(UriReader(), SnakeYamlConverter())
-        store.addSchema(
-            URI.create("http://json-schema.org/draft-04/schema#"),
-            "/json-schema/draft-04/schema.json")
+//        val printer = ListResultBuilder(FullResultTextBuilder())
+//        val messages = printer.print(result.validationMessages)
+//        print(messages)
 
-        val x = result.validate(validator, store)
-
-        x.shouldBeTrue()
+        valid.shouldBeTrue()
     }
 
 })
