0ed8a8609b4c2a551d73602c2c65cc374b01b9e3
Martin Hauner <martin.hauner@gmx.net>
2022-06-05 10:41:52 +0200

    enable

--- openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/Draft7Spec.kt
+++ openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/Draft7Spec.kt
@@ -5,11 +5,9 @@
 
 package io.openapiparser.validator
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
 import io.openapiparser.validator.support.*
 
-@Ignored
 class Draft7Spec: FreeSpec ({
     include(draftSpec(
         "/suites/JSON-Schema-Test-Suite/tests/draft7",
@@ -19,17 +17,29 @@ class Draft7Spec: FreeSpec ({
 })
 
 val draft7Extras = listOf(
-    // (optional) json-pointer.json, not implemented
+    // (optional format) not implemented
+    Exclude("idn-email.json"),
+    // (optional format) not implemented
+    Exclude("idn-hostname.json"),
+    // (optional format) not implemented
+    Exclude("iri.json"),
+    // (optional format) not implemented
+    Exclude("iri-reference.json"),
+    // (optional format) not implemented
     Exclude("json-pointer.json"),
-    // (optional) uri-template.json, not implemented
+    // (optional format) not implemented
+    Exclude("relative-json-pointer.json"),
+    // (optional format) not implemented
     Exclude("uri-template.json"),
+    // (optional) content.json, not implemented
+    Exclude("content.json"),
     // (optional) ecmascript-regex.json, too many fails to list them
     Exclude("ecmascript-regex.json"),
     // (optional) date-time.json, supports all except leap second
     Exclude("a valid date-time with a leap second, UTC"),
     Exclude("a valid date-time with a leap second, with minus offset"),
     // (optional) time.json, supports all except leap second
-    Exclude("a valid time string with a leap second, Zulu"),
+    Exclude("a valid time string with leap second, Zulu"),
     Exclude("valid leap second, zero time-offset"),
     Exclude("valid leap second, positive time-offset"),
     Exclude("valid leap second, large positive time-offset"),
