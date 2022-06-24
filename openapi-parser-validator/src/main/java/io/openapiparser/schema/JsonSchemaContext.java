12d682569bb73fdddcd0667da15de9c6ca506f92
Martin Hauner <martin.hauner@gmx.net>
2022-06-24 14:06:00 +0200

    clean up

--- openapi-parser-validator/src/main/java/io/openapiparser/schema/JsonSchemaContext.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/schema/JsonSchemaContext.java
@@ -10,6 +10,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import java.net.URI;
 import java.util.Map;
 
+import static io.openapiparser.schema.ScopeSupport.updateScope;
+
 public class JsonSchemaContext {
     private final SchemaVersion version;
     private final URI scope;
@@ -39,14 +41,6 @@ public class JsonSchemaContext {
         return references.getRef (resolved);
     }
 
-    @Deprecated
-    public JsonSchemaContext withSource (URI source) {
-        if (scope.equals (source)) {
-            return this;
-        }
-        return new JsonSchemaContext (source, references, version);
-    }
-
     public JsonSchemaContext withScope (@Nullable URI targetScope) {
         // todo possible??
         if (targetScope == null) {
@@ -56,18 +50,9 @@ public class JsonSchemaContext {
     }
 
     public JsonSchemaContext withId (Map<String, Object> properties) {
-        String id = version.getIdProvider ().getId (properties);
-        if (id == null) {
-            return this;
-        }
-
-        return withId (id);
-    }
-
-    public JsonSchemaContext withId (@Nullable String id) {
-        if (id == null) {
-            return this;
-        }
-        return new JsonSchemaContext (scope.resolve (id), references, version);
+        return new JsonSchemaContext (
+            updateScope (properties, scope, version.getIdProvider ()),
+            references,
+            version);
     }
 }
