496bd860c0d75f9217ef294fdcf7cf8b34ef0150
Martin Hauner <martin.hauner@gmx.net>
2023-03-04 09:53:10 +0100

    extract document loader

---
+++ openapi-parser-validator/src/main/java/io/openapiparser/schema/DocumentLoader.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 https://github.com/openapi-processor/openapi-parser
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiparser.schema;
+
+import io.openapiparser.Converter;
+import io.openapiparser.Reader;
+import io.openapiparser.support.Strings;
+
+import java.io.InputStream;
+import java.net.URI;
+
+import static io.openapiparser.support.Nullness.nonNull;
+
+public class DocumentLoader {
+    private final Reader reader;
+    private final Converter converter;
+
+    public DocumentLoader (Reader reader, Converter converter) {
+        this.reader = reader;
+        this.converter = converter;
+    }
+
+    public Object loadDocument (URI documentUri) {
+        try {
+            return converter.convert (Strings.of (reader.read (documentUri)));
+        } catch (Exception e) {
+            throw new DocumentLoaderException (String.format ("failed to download '%s'.", documentUri), e);
+        }
+    }
+
+    public Object loadDocument (String resourcePath) {
+        try {
+            InputStream source = nonNull (getClass ().getResourceAsStream (resourcePath));
+            return converter.convert (Strings.of (source));
+        } catch (Exception e) {
+            throw new DocumentLoaderException (String.format ("failed to load '%s'.", resourcePath), e);
+        }
+    }
+}
