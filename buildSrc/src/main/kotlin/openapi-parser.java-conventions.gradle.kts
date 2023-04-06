540a072e17a212dd47d5ae00ef07e232f07abde9
Martin Hauner <martin.hauner@gmx.net>
2023-04-06 16:05:19 +0200

    clean up

--- buildSrc/src/main/kotlin/openapi-parser.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/openapi-parser.java-conventions.gradle.kts
@@ -42,7 +42,7 @@ java {
 
 kotlin {
     jvmToolchain {
-        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(11))
+        this.languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
