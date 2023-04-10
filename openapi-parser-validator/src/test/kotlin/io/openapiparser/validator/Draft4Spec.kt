4b251c5fa9d1e75824459b789b46ede28e2facd0
Martin Hauner <martin.hauner@gmx.net>
2023-04-10 10:30:06 +0200

    obsolete

--- openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/Draft4Spec.kt
+++ openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/Draft4Spec.kt
@@ -20,55 +20,5 @@ val draft4Extras = listOf(
     Exclude("ecmascript-regex.json"),
     // (optional) date-time.json, supports all except leap second
     Exclude("a valid date-time with a leap second, UTC"),
-    Exclude("a valid date-time with a leap second, with minus offset"),
-
-    // remote documents
-    Remote(
-        "remote ref", Document(
-            "http://localhost:1234/integer.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/integer.json"
-        )
-    ),
-    Remote(
-        "fragment within remote ref", Document(
-            "http://localhost:1234/subSchemas.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/subSchemas.json"
-        )
-    ),
-    Remote(
-        "ref within remote ref", Document(
-            "http://localhost:1234/subSchemas.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/subSchemas.json"
-        )
-    ),
-    Remote(
-        "base URI change", Document(
-            "http://localhost:1234/baseUriChange/folderInteger.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/baseUriChange/folderInteger.json"
-        )
-    ),
-    Remote(
-        "base URI change - change folder", Document(
-            "http://localhost:1234/baseUriChangeFolder/folderInteger.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/baseUriChangeFolder/folderInteger.json"
-        )
-    ),
-    Remote(
-        "base URI change - change folder in subschema", Document(
-            "http://localhost:1234/baseUriChangeFolderInSubschema/folderInteger.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/baseUriChangeFolderInSubschema/folderInteger.json"
-        )
-    ),
-    Remote(
-        "root ref in remote ref", Document(
-            "http://localhost:1234/name.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/name.json"
-        )
-    ),
-    Remote(
-        "Location-independent identifier in remote ref", Document(
-            "http://localhost:1234/locationIndependentIdentifierDraft4.json",
-            "/suites/JSON-Schema-Test-Suite/remotes/locationIndependentIdentifierDraft4.json"
-        )
-    )
+    Exclude("a valid date-time with a leap second, with minus offset")
 )
