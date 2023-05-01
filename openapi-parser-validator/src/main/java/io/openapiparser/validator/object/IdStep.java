a4a23a50f4d47206a0140553829a7642105de430
Martin Hauner <martin.hauner@gmx.net>
2023-05-01 12:05:13 +0200

    rework steps

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/object/IdStep.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/object/IdStep.java
@@ -7,13 +7,14 @@ package io.openapiparser.validator.object;
 
 import io.openapiparser.schema.JsonInstance;
 import io.openapiparser.schema.JsonSchema;
+import io.openapiparser.schema.Keywords;
 import io.openapiparser.validator.ValidationMessage;
 import io.openapiparser.validator.steps.SimpleStep;
 
 public class IdStep extends SimpleStep {
 
     public IdStep (JsonSchema schema, JsonInstance instance) {
-        super(schema, instance);
+        super(schema, instance, Keywords.ID);
     }
 
     @Override
