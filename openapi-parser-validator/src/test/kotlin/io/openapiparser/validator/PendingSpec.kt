d3ea8d5bf7d3201578c64db2f350857e03ef9f29
Martin Hauner <martin.hauner@gmx.net>
2023-05-07 17:04:36 +0200

    enable all formats

--- openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/PendingSpec.kt
+++ openapi-parser-validator/src/test/kotlin/io/openapiparser/validator/PendingSpec.kt
@@ -7,12 +7,17 @@ package io.openapiparser.validator
 
 import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FreeSpec
+import io.openapiparser.schema.Format
 import io.openapiparser.schema.SchemaVersion
 import io.openapiparser.validator.support.draftSpec
 
 @Ignored
 class PendingSpec : FreeSpec({
-    val settings = ValidatorSettings()
+//    val settings = ValidatorSettingsDefaults.draft4()
+//    val settings = ValidatorSettingsDefaults.draft7()
+//    val settings = ValidatorSettingsDefaults.draft201909()
+    val settings = ValidatorSettingsDefaults.draft202012()
+    settings.enableFormats(*Format.values())
 //    settings.version = SchemaVersion.Draft4
 //    settings.version = SchemaVersion.Draft6
 //    settings.version = SchemaVersion.Draft7
