3a01138232335f16acde3797f8920f69219dc330
Martin Hauner <martin.hauner@gmx.net>
2023-05-07 15:54:29 +0200

    handle format annotation/assertion

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/string/Email.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/string/Email.java
@@ -7,10 +7,10 @@ package io.openapiparser.validator.string;
 
 import io.openapiparser.schema.JsonInstance;
 import io.openapiparser.schema.JsonSchema;
+import io.openapiparser.schema.Vocabularies;
 import io.openapiparser.validator.ValidatorSettings;
 import io.openapiparser.validator.steps.ValidationStep;
 import io.openapiparser.validator.support.EmailValidator;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import static io.openapiparser.schema.Format.EMAIL;
 import static io.openapiparser.support.Nullness.nonNull;
@@ -19,7 +19,6 @@ import static io.openapiparser.support.Nullness.nonNull;
  * validates email. Since Draft 4.
  */
 public class Email {
-
     private final ValidatorSettings settings;
 
     public Email (ValidatorSettings settings) {
@@ -27,28 +26,56 @@ public class Email {
     }
 
     public void validate (JsonSchema schema, JsonInstance instance, ValidationStep parentStep) {
-        String format = schema.getFormat ();
-        if (!shouldValidate (format))
+        if (!hasFormat (schema))
             return;
 
         EmailStep step = new EmailStep (schema, instance);
 
-        String instanceValue = getInstanceValue (instance);
-        boolean valid = new EmailValidator (instanceValue).validate ();
+        if (shouldValidate (schema)) {
+            String instanceValue = getInstanceValue (instance);
+            boolean valid = new EmailValidator (instanceValue).validate ();
 
-        if (!valid)
-            step.setInvalid ();
+            if (!valid)
+                step.setInvalid ();
+        }
+
+        if (shouldAnnotate (schema)) {
+            step.createAnnotation ();
+        }
 
         parentStep.add (step);
     }
 
-    private boolean shouldValidate (@Nullable String format) {
-        return format != null
-            && format.equals (EMAIL.getFormat ())
+    private boolean shouldAnnotate (JsonSchema schema) {
+        return getVocabularies (schema).hasFormatAnnotation ();
+    }
+
+    private boolean shouldValidate (JsonSchema schema) {
+        return getVocabularies (schema).hasFormatAssertion ()
             && settings.validateFormat (EMAIL);
+    }
+
+    private boolean hasFormat (JsonSchema schema) {
+        String format = schema.getFormat ();
+        return format != null
+            && format.equals (EMAIL.getFormat ());
     }
 
     private String getInstanceValue (JsonInstance instance) {
         return nonNull (instance.asString ());
+    }
+
+    private Vocabularies getVocabularies (JsonSchema schema) {
+        JsonSchema metaSchema = schema.getMetaSchemaSchema ();
+        if (metaSchema == null) {
+            return Vocabularies.ALL;
+        }
+
+        Vocabularies vocabularies = metaSchema.getVocabulary ();
+        if (vocabularies == null) {
+            return Vocabularies.ALL;
+        }
+
+        return vocabularies;
     }
 }
--- openapi-parser-validator/src/main/java/io/openapiparser/validator/string/EmailStep.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/string/EmailStep.java
@@ -8,10 +8,13 @@ package io.openapiparser.validator.string;
 import io.openapiparser.schema.JsonInstance;
 import io.openapiparser.schema.JsonSchema;
 import io.openapiparser.schema.Keywords;
+import io.openapiparser.validator.Annotation;
 import io.openapiparser.validator.ValidationMessage;
 import io.openapiparser.validator.steps.SimpleStep;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class EmailStep extends SimpleStep {
+    private @Nullable Annotation annotation;
 
     public EmailStep (JsonSchema schema, JsonInstance instance) {
         super(schema, instance, Keywords.FORMAT);
@@ -20,5 +23,14 @@ public class EmailStep extends SimpleStep {
     @Override
     protected ValidationMessage getError () {
         return new EmailError (schema, instance);
+    }
+
+    @Override
+    public @Nullable Annotation getAnnotation () {
+        return annotation;
+    }
+
+    public void createAnnotation () {
+        annotation = new Annotation (Keywords.FORMAT, schema.getFormat ());
     }
 }
