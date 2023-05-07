8b07813471a332058e0ea4577d7d0bc4f3b28a00
Martin Hauner <martin.hauner@gmx.net>
2023-05-07 11:49:53 +0200

    fix regex format

--- openapi-parser-validator/src/main/java/io/openapiparser/validator/string/Regex.java
+++ openapi-parser-validator/src/main/java/io/openapiparser/validator/string/Regex.java
@@ -7,30 +7,46 @@ package io.openapiparser.validator.string;
 
 import io.openapiparser.schema.JsonInstance;
 import io.openapiparser.schema.JsonSchema;
+import io.openapiparser.validator.ValidatorSettings;
 import io.openapiparser.validator.steps.ValidationStep;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
+import java.util.regex.Pattern;
+
+import static io.openapiparser.schema.Format.REGEX;
 import static io.openapiparser.support.Nullness.nonNull;
 
 /**
  * validates regex format. Since Draft 7.
  */
 public class Regex {
+    private final ValidatorSettings settings;
+
+    public Regex (ValidatorSettings settings) {
+        this.settings = settings;
+    }
 
     public void validate (JsonSchema schema, JsonInstance instance, ValidationStep parentStep) {
-        String pattern = schema.getPattern ();
-        if (pattern == null)
+        String format = schema.getFormat ();
+        if (!shouldValidate (format))
             return;
 
         RegexStep step = new RegexStep (schema, instance);
 
         try {
-            java.util.regex.Pattern.compile(getInstanceValue (instance));
+            Pattern.compile (getInstanceValue (instance));
 
         } catch (Exception ex) {
             step.setInvalid ();
         }
 
         parentStep.add (step);
+    }
+
+    private boolean shouldValidate (@Nullable String format) {
+        return format != null
+            && format.equals (REGEX.getFormat ())
+            && settings.validateFormat (REGEX);
     }
 
     private String getInstanceValue (JsonInstance instance) {
