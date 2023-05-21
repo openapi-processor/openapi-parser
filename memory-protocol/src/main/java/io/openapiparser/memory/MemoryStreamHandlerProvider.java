de02d09e0c3d07cdd6f011164a6beb8929109438
Martin Hauner <martin.hauner@gmx.net>
2023-05-21 16:03:00 +0200

    re-activate jdk9+ code

--- memory-protocol/src/main/java/io/openapiparser/memory/MemoryStreamHandlerProvider.java
+++ memory-protocol/src/main/java/io/openapiparser/memory/MemoryStreamHandlerProvider.java
@@ -7,19 +7,19 @@ package io.openapiparser.memory;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-//import java.net.URLStreamHandler;
-//import java.net.spi.URLStreamHandlerProvider;
+import java.net.URLStreamHandler;
+import java.net.spi.URLStreamHandlerProvider;
 
 /**
  * Simple in-memory protocol for "loading" content from memory via URL. JDK-9+ version
  */
-//public class MemoryStreamHandlerProvider extends URLStreamHandlerProvider {
-//
-//    @Override
-//    public @Nullable URLStreamHandler createURLStreamHandler (String protocol) {
-//        if (!"memory".equals (protocol))
-//            return null;
-//
-//        return new MemoryUrlStreamHandler ();
-//    }
-//}
+public class MemoryStreamHandlerProvider extends URLStreamHandlerProvider {
+
+    @Override
+    public @Nullable URLStreamHandler createURLStreamHandler (String protocol) {
+        if (!"memory".equals (protocol))
+            return null;
+
+        return new MemoryUrlStreamHandler ();
+    }
+}
