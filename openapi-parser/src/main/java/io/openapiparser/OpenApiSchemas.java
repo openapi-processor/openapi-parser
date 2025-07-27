061b70a19294c007ecac36bf040c52c0470af9c1
Martin Hauner <martin.hauner@gmx.net>
2025-07-27 10:53:43 +0200

    update schema id

--- openapi-parser/src/main/java/io/openapiparser/OpenApiSchemas.java
+++ openapi-parser/src/main/java/io/openapiparser/OpenApiSchemas.java
@@ -11,7 +11,7 @@ public class OpenApiSchemas {
     static URI    OPENAPI_SCHEMA_30_ID = id("https://spec.openapis.org/oas/3.0/schema/2021-09-28");
     static String OPENAPI_SCHEMA_30 = "/openapi/schemas/v3.0/schema.yaml";
 
-    static URI    OPENAPI_SCHEMA_31_ID = id("https://spec.openapis.org/oas/3.1/schema/2022-02-27");
+    static URI    OPENAPI_SCHEMA_31_ID = id("https://spec.openapis.org/oas/3.1/schema/2022-10-07");
     static String OPENAPI_SCHEMA_31 = "/openapi/schemas/v3.1/schema.yaml";
 
     static URI    OVERLAY_SCHEMA_10_ID = id("https://spec.openapis.org/overlay/1.0/schema");
