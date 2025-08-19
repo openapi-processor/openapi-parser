f1a54a3e2b285d63ecadef3243dc7eeaff9752af
Martin Hauner <martin.hauner@gmx.net>
2025-08-19 15:37:28 +0200

    fix incomplete javadoc

--- json-schema-validator/src/main/java/io/openapiprocessor/jsonschema/schema/Resolver.java
+++ json-schema-validator/src/main/java/io/openapiprocessor/jsonschema/schema/Resolver.java
@@ -55,6 +55,7 @@ public class Resolver {
         /**
          * get a base uri from a document if available.
          *
+         * @param documentUri document url
          * @param document the document
          * @return base uri or null
          */
