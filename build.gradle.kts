255a24d6000a20a7bb6296920ce57af8bc807fc7
Martin Hauner <martin.hauner@gmx.net>
2025-09-10 12:53:09 +0200

    publish all

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     base
     alias(libs.plugins.jacoco)
+    id("io.openapiprocessor.build.plugin.publish-central")
 }
 
 repositories {
@@ -15,3 +16,9 @@ tasks.named("build") {
 group = "io.openapiprocessor"
 version = libs.versions.openapiparser.get()
 println("version: $version")
+
+publishProcessor {
+    stagingDir = layout.buildDirectory.dir("staging")
+    deploymentDir = layout.buildDirectory.dir("deployment")
+    publish = false
+}
